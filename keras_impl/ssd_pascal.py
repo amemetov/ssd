f2714479d5f2f706b76c3a17527e8551c7795111
Arsen <arsen.memetov@aspose.com>
2018-01-31 14:22:39 +0200

    Reworking: storing tensors in the dict to get eas access to the necessary tensor when need.

--- keras_impl/ssd_pascal.py
+++ keras_impl/ssd_pascal.py
@@ -10,21 +10,37 @@ from keras.optimizers import Adam
 # from keras.applications import VGG16
 from .vgg import VGG16
 
-def SSD300(base_net_fine_tune_start_layer='conv4_1', use_bn=False):
-    # VGG16 - Input size must be at least 48x48;
-    # SSD 300 - input size 300x300
+
+def SSD300(base_net_name='vgg16', freeze_layers=None, use_bn=False):
+    if base_net_name not in {'vgg16', None}:
+        raise ValueError('The `base_net_name` argument should be either '
+                         '`vgg16` or (other nets will be added in the future).')
+
     input_dim = (300, 300, 3)
 
-    image_input = Input(shape=input_dim, name='image_input')
+    ssd_net = {}
 
-    base_net = VGG16(include_top=False, weights='imagenet',
-                      input_tensor=image_input, input_shape=input_dim)
+    ssd_net['image_input'] = Input(shape=input_dim, name='image_input')
 
-    _freezeBaseNet(base_net, base_net_fine_tune_start_layer)
+    base_net_model = VGG16(ssd_net, ssd_net['image_input'], input_shape=input_dim,
+                           include_top=False, weights='imagenet')
 
-    predictions = addExtraLayers(base_net, use_bn=use_bn)
+    # predictions = addExtraLayers(base_net, use_bn=use_bn)
+    addExtraLayers(ssd_net, base_net_model, use_bn=use_bn)
 
-    model = Model(inputs=[image_input], outputs=[predictions])
+    ssd_net['predictions'] = ssd_net['conv9_2']
+
+    mbox_source_layers = ['conv4_3', 'fc7', 'conv6_2', 'conv7_2', 'conv8_2', 'conv9_2']
+
+    # mbox_layers = CreateMultiBoxHead(net, data_layer='data', from_layers=mbox_source_layers,
+    #                                  use_batchnorm=use_batchnorm, min_sizes=min_sizes, max_sizes=max_sizes,
+    #                                  aspect_ratios=aspect_ratios, steps=steps, normalizations=normalizations,
+    #                                  num_classes=num_classes, share_location=share_location, flip=flip, clip=clip,
+    #                                  prior_variance=prior_variance, kernel_size=3, pad=1, lr_mult=lr_mult)
+
+    model = Model(inputs=[ssd_net['image_input']], outputs=[ssd_net['predictions']])
+
+    _freezeLayers(model, freeze_layers)
 
     return model
 
@@ -46,63 +62,216 @@ def SSD300(base_net_fine_tune_start_layer='conv4_1', use_bn=False):
 
 
 # Add extra layers on top of a "base" network (e.g. VGGNet or Inception).
-def addExtraLayers(base_net, use_bn=True,  use_dropout=False):
+def addExtraLayers(ssd_net, base_net_model, use_bn=True, use_dropout=False):
     # Add additional convolutional layers.
 
-    x = base_net.layers[-1].output
+    x = base_net_model.layers[-1].output
 
     # 19 x 19
     # FC6
-    x = Conv2D(1024, 3, padding='same', dilation_rate=(6, 6), activation='relu', name='fc6')(x)
+    ssd_net['fc6'] = x = Conv2D(1024, 3, padding='same', dilation_rate=(6, 6), activation='relu', name='fc6')(x)
     if use_dropout:
-        x = Dropout(0.5, name='drop6')(x)
+        ssd_net['drop6'] = x = Dropout(0.5, name='drop6')(x)
 
     # FC7
-    x = Conv2D(1024, 1, padding='same', activation='relu', name='fc7')(x)
+    ssd_net['fc7'] = x = Conv2D(1024, 1, padding='same', activation='relu', name='fc7')(x)
     if use_dropout:
-        x = Dropout(0.5, name='drop7')(x)
-
+        ssd_net['drop7'] = x = Dropout(0.5, name='drop7')(x)
 
     # 10 x 10
-    x = ConvBNLayer(x, "conv6_1", use_bn, 256, 1, 'valid', 1)
-    x = ConvBNLayer(x, "conv6_2", use_bn, 512, 3, 'same', 2)
+    x = ConvBNLayer(ssd_net, x, "conv6_1", use_bn, 256, 1, 'valid', 1)
+    x = ConvBNLayer(ssd_net, x, "conv6_2", use_bn, 512, 3, 'same', 2)
 
     # 5 x 5
-    x = ConvBNLayer(x, "conv7_1", use_bn, 128, 1, 'valid', 1)
-    x = ConvBNLayer(x, "conv7_2", use_bn, 256, 3, 'same', 2)
+    x = ConvBNLayer(ssd_net, x, "conv7_1", use_bn, 128, 1, 'valid', 1)
+    x = ConvBNLayer(ssd_net, x, "conv7_2", use_bn, 256, 3, 'same', 2)
 
     # 3 x 3
-    x = ConvBNLayer(x, "conv8_1", use_bn, 128, 1, 'valid', 1)
-    x = ConvBNLayer(x, "conv8_2", use_bn, 256, 3, 'valid', 1)
+    x = ConvBNLayer(ssd_net, x, "conv8_1", use_bn, 128, 1, 'valid', 1)
+    x = ConvBNLayer(ssd_net, x, "conv8_2", use_bn, 256, 3, 'valid', 1)
 
     # 1 x 1
-    x = ConvBNLayer(x, "conv9_1", use_bn, 128, 1, 'valid', 1)
-    x = ConvBNLayer(x, "conv9_2", use_bn, 256, 3, 'valid', 1)
-
-    return x
+    x = ConvBNLayer(ssd_net, x, "conv9_1", use_bn, 128, 1, 'valid', 1)
+    x = ConvBNLayer(ssd_net, x, "conv9_2", use_bn, 256, 3, 'valid', 1)
 
 
-def ConvBNLayer(net, layer_name, use_bn,
+def ConvBNLayer(net, tensor, layer_name, use_bn,
                 num_output, kernel_size, pad, stride,
                 dilation=1):
-    x = Conv2D(filters=num_output, kernel_size=kernel_size,
-                      strides=stride,
-                      padding=pad,
-                      dilation_rate=dilation,
-                      activation='relu',
-                      name=layer_name)(net)
+    net[layer_name] = x = Conv2D(filters=num_output, kernel_size=kernel_size,
+                                 strides=stride,
+                                 padding=pad,
+                                 dilation_rate=dilation,
+                                 activation='relu',
+                                 name=layer_name)(tensor)
     if use_bn:
-        x = BatchNormalization(name=layer_name+'_bn')(x)
+        net[layer_name + '_bn'] = x = BatchNormalization(name=layer_name + '_bn')(x)
 
     return x
 
 
-def _freezeBaseNet(conv_base, fine_tune_start_layer='conv4_1'):
-    conv_base.trainable = True
-    set_trainable = False
-    for layer in conv_base.layers:
-        if layer.name == fine_tune_start_layer:
-            set_trainable = True
+def CreateMultiBoxHead(net, data_layer="data", num_classes=[], from_layers=[],
+                       use_objectness=False, normalizations=[], use_batchnorm=True, lr_mult=1,
+                       use_scale=True, min_sizes=[], max_sizes=[], prior_variance=[0.1],
+                       aspect_ratios=[], steps=[], img_height=0, img_width=0, share_location=True,
+                       flip=True, clip=True, offset=0.5, inter_layer_depth=[], kernel_size=1, pad=0,
+                       conf_postfix='', loc_postfix='', **bn_param):
+    assert num_classes, "must provide num_classes"
+    assert num_classes > 0, "num_classes must be positive number"
+    if normalizations:
+        assert len(from_layers) == len(normalizations), "from_layers and normalizations should have same length"
+    assert len(from_layers) == len(min_sizes), "from_layers and min_sizes should have same length"
+    if max_sizes:
+        assert len(from_layers) == len(max_sizes), "from_layers and max_sizes should have same length"
+    if aspect_ratios:
+        assert len(from_layers) == len(aspect_ratios), "from_layers and aspect_ratios should have same length"
+    if steps:
+        assert len(from_layers) == len(steps), "from_layers and steps should have same length"
+    net_layers = net.keys()
+    assert data_layer in net_layers, "data_layer is not in net's layers"
+    if inter_layer_depth:
+        assert len(from_layers) == len(inter_layer_depth), "from_layers and inter_layer_depth should have same length"
 
-        layer.trainable = set_trainable
+    num = len(from_layers)
+    priorbox_layers = []
+    loc_layers = []
+    conf_layers = []
+    objectness_layers = []
+    for i in range(0, num):
+        from_layer = from_layers[i]
 
+        # Get the normalize value.
+        if normalizations:
+            if normalizations[i] != -1:
+                norm_name = "{}_norm".format(from_layer)
+                net[norm_name] = L.Normalize(net[from_layer],
+                                             scale_filler=dict(type="constant", value=normalizations[i]),
+                                             across_spatial=False, channel_shared=False)
+                from_layer = norm_name
+
+        # Add intermediate layers.
+        if inter_layer_depth:
+            if inter_layer_depth[i] > 0:
+                inter_name = "{}_inter".format(from_layer)
+                ConvBNLayer(net, from_layer, inter_name, use_bn=use_batchnorm, use_relu=True, lr_mult=lr_mult,
+                            num_output=inter_layer_depth[i], kernel_size=3, pad=1, stride=1, **bn_param)
+                from_layer = inter_name
+
+        # Estimate number of priors per location given provided parameters.
+        min_size = min_sizes[i]
+        if type(min_size) is not list:
+            min_size = [min_size]
+        aspect_ratio = []
+        if len(aspect_ratios) > i:
+            aspect_ratio = aspect_ratios[i]
+            if type(aspect_ratio) is not list:
+                aspect_ratio = [aspect_ratio]
+        max_size = []
+        if len(max_sizes) > i:
+            max_size = max_sizes[i]
+            if type(max_size) is not list:
+                max_size = [max_size]
+            if max_size:
+                assert len(max_size) == len(min_size), "max_size and min_size should have same length."
+        if max_size:
+            num_priors_per_location = (2 + len(aspect_ratio)) * len(min_size)
+        else:
+            num_priors_per_location = (1 + len(aspect_ratio)) * len(min_size)
+        if flip:
+            num_priors_per_location += len(aspect_ratio) * len(min_size)
+        step = []
+        if len(steps) > i:
+            step = steps[i]
+
+        # Create location prediction layer.
+        name = "{}_mbox_loc{}".format(from_layer, loc_postfix)
+        num_loc_output = num_priors_per_location * 4;
+        if not share_location:
+            num_loc_output *= num_classes
+        ConvBNLayer(net, from_layer, name, use_bn=use_batchnorm, use_relu=False, lr_mult=lr_mult,
+                    num_output=num_loc_output, kernel_size=kernel_size, pad=pad, stride=1, **bn_param)
+        permute_name = "{}_perm".format(name)
+        net[permute_name] = L.Permute(net[name], order=[0, 2, 3, 1])
+        flatten_name = "{}_flat".format(name)
+        net[flatten_name] = L.Flatten(net[permute_name], axis=1)
+        loc_layers.append(net[flatten_name])
+
+        # Create confidence prediction layer.
+        name = "{}_mbox_conf{}".format(from_layer, conf_postfix)
+        num_conf_output = num_priors_per_location * num_classes;
+        ConvBNLayer(net, from_layer, name, use_bn=use_batchnorm, use_relu=False, lr_mult=lr_mult,
+                    num_output=num_conf_output, kernel_size=kernel_size, pad=pad, stride=1, **bn_param)
+        permute_name = "{}_perm".format(name)
+        net[permute_name] = L.Permute(net[name], order=[0, 2, 3, 1])
+        flatten_name = "{}_flat".format(name)
+        net[flatten_name] = L.Flatten(net[permute_name], axis=1)
+        conf_layers.append(net[flatten_name])
+
+        # Create prior generation layer.
+        name = "{}_mbox_priorbox".format(from_layer)
+        net[name] = L.PriorBox(net[from_layer], net[data_layer], min_size=min_size,
+                               clip=clip, variance=prior_variance, offset=offset)
+        if max_size:
+            net.update(name, {'max_size': max_size})
+        if aspect_ratio:
+            net.update(name, {'aspect_ratio': aspect_ratio, 'flip': flip})
+        if step:
+            net.update(name, {'step': step})
+        if img_height != 0 and img_width != 0:
+            if img_height == img_width:
+                net.update(name, {'img_size': img_height})
+            else:
+                net.update(name, {'img_h': img_height, 'img_w': img_width})
+        priorbox_layers.append(net[name])
+
+        # Create objectness prediction layer.
+        if use_objectness:
+            name = "{}_mbox_objectness".format(from_layer)
+            num_obj_output = num_priors_per_location * 2;
+            ConvBNLayer(net, from_layer, name, use_bn=use_batchnorm, use_relu=False, lr_mult=lr_mult,
+                        num_output=num_obj_output, kernel_size=kernel_size, pad=pad, stride=1, **bn_param)
+            permute_name = "{}_perm".format(name)
+            net[permute_name] = L.Permute(net[name], order=[0, 2, 3, 1])
+            flatten_name = "{}_flat".format(name)
+            net[flatten_name] = L.Flatten(net[permute_name], axis=1)
+            objectness_layers.append(net[flatten_name])
+
+    # Concatenate priorbox, loc, and conf layers.
+    mbox_layers = []
+    name = "mbox_loc"
+    net[name] = L.Concat(*loc_layers, axis=1)
+    mbox_layers.append(net[name])
+    name = "mbox_conf"
+    net[name] = L.Concat(*conf_layers, axis=1)
+    mbox_layers.append(net[name])
+    name = "mbox_priorbox"
+    net[name] = L.Concat(*priorbox_layers, axis=2)
+    mbox_layers.append(net[name])
+    if use_objectness:
+        name = "mbox_objectness"
+        net[name] = L.Concat(*objectness_layers, axis=1)
+        mbox_layers.append(net[name])
+
+    return mbox_layers
+
+
+def _freezeLayers(model, freeze_layers):
+    model.trainable = True
+    if freeze_layers is None:
+        freeze_layers = ['conv1_1', 'conv1_2', 'pool1',
+                         'conv2_1', 'conv2_2', 'pool2',
+                         'conv3_1', 'conv3_2', 'conv3_3', 'pool3',
+                         # 'conv4_1', 'conv4_2', 'conv4_3', 'pool4',
+                         # 'conv5_1', 'conv5_2', 'conv5_3', 'pool5'
+                         ]
+    for layer in model.layers:
+        layer.trainable = layer.name not in freeze_layers
+
+# def _freezeBaseNet(conv_base, fine_tune_start_layer='conv4_1'):
+#     conv_base.trainable = True
+#     set_trainable = False
+#     for layer in conv_base.layers:
+#         if layer.name == fine_tune_start_layer:
+#             set_trainable = True
+#
+#         layer.trainable = set_trainable
--- keras_impl/vgg.py
+++ keras_impl/vgg.py
@@ -28,8 +28,8 @@ from keras.engine.topology import get_source_inputs
 from keras.utils import layer_utils
 from keras.utils.data_utils import get_file
 from keras import backend as K
-from keras.applications.imagenet_utils import decode_predictions
-from keras.applications.imagenet_utils import preprocess_input
+#from keras.applications.imagenet_utils import decode_predictions
+#from keras.applications.imagenet_utils import preprocess_input
 from keras.applications.imagenet_utils import _obtain_input_shape
 
 
@@ -37,9 +37,9 @@ WEIGHTS_PATH = 'https://github.com/fchollet/deep-learning-models/releases/downlo
 WEIGHTS_PATH_NO_TOP = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
 
 
-def VGG16(include_top=True, weights='imagenet',
-          input_tensor=None, input_shape=None,
-          pooling=None,
+def VGG16(net, input_tensor=None, input_shape=None,
+          include_top=True, weights='imagenet',
+          #pooling=None,
           classes=1000):
     """Instantiates the VGG16 architecture.
 
@@ -98,6 +98,7 @@ def VGG16(include_top=True, weights='imagenet',
     if weights == 'imagenet' and include_top and classes != 1000:
         raise ValueError('If using `weights` as imagenet with `include_top`'
                          ' as true, `classes` should be 1000')
+
     # Determine proper input shape
     input_shape = _obtain_input_shape(input_shape,
                                       default_size=224,
@@ -107,56 +108,56 @@ def VGG16(include_top=True, weights='imagenet',
                                       weights=weights)
 
     if input_tensor is None:
-        img_input = Input(shape=input_shape)
+        net['image_input'] = img_input = Input(shape=input_shape)
     else:
         if not K.is_keras_tensor(input_tensor):
-            img_input = Input(tensor=input_tensor, shape=input_shape)
+            net['image_input'] = img_input = Input(tensor=input_tensor, shape=input_shape)
         else:
             img_input = input_tensor
     # Block 1
-    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(img_input)
-    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+    net['conv1_1'] = x = Conv2D(64, (3, 3), activation='relu', padding='same', name='conv1_1')(img_input)
+    net['conv1_2'] = x = Conv2D(64, (3, 3), activation='relu', padding='same', name='conv1_2')(x)
     # x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), padding='same', name='block1_pool')(x)
+    net['pool1'] = x = MaxPooling2D((2, 2), strides=(2, 2), padding='same', name='pool1')(x)
 
     # Block 2
-    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
-    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
+    net['conv2_1'] = x = Conv2D(128, (3, 3), activation='relu', padding='same', name='conv2_1')(x)
+    net['conv2_2'] = x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
     #x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), padding='same', name='block2_pool')(x)
+    net['pool2'] = x = MaxPooling2D((2, 2), strides=(2, 2), padding='same', name='pool2')(x)
 
     # Block 3
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
+    net['conv3_1'] = x = Conv2D(256, (3, 3), activation='relu', padding='same', name='conv3_1')(x)
+    net['conv3_2'] = x = Conv2D(256, (3, 3), activation='relu', padding='same', name='conv3_2')(x)
+    net['conv3_3'] = x = Conv2D(256, (3, 3), activation='relu', padding='same', name='conv3_3')(x)
     #x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), padding='same', name='block3_pool')(x)
+    net['pool3'] = x = MaxPooling2D((2, 2), strides=(2, 2), padding='same', name='pool3')(x)
 
     # Block 4
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
+    net['conv4_1'] = x = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv4_1')(x)
+    net['conv4_2'] = x = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv4_2')(x)
+    net['conv4_3'] = x = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv4_3')(x)
     # x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), padding='same', name='block4_pool')(x)
+    net['pool4'] = x = MaxPooling2D((2, 2), strides=(2, 2), padding='same', name='pool4')(x)
 
     # Block 5
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
+    net['conv5_1'] = x = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv5_1')(x)
+    net['conv5_2'] = x = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv5_2')(x)
+    net['conv5_3'] = x = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv5_3')(x)
     #x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
-    x = MaxPooling2D((3, 3), strides=(1, 1), padding='same', name='block5_pool')(x)
+    net['pool5'] = x = MaxPooling2D((3, 3), strides=(1, 1), padding='same', name='pool5')(x)
 
     if include_top:
         # Classification block
-        x = Flatten(name='flatten')(x)
-        x = Dense(4096, activation='relu', name='fc1')(x)
-        x = Dense(4096, activation='relu', name='fc2')(x)
-        x = Dense(classes, activation='softmax', name='predictions')(x)
-    else:
-        if pooling == 'avg':
-            x = GlobalAveragePooling2D()(x)
-        elif pooling == 'max':
-            x = GlobalMaxPooling2D()(x)
+        net['flatten'] = x = Flatten(name='flatten')(x)
+        net['fc1'] = x = Dense(4096, activation='relu', name='fc1')(x)
+        net['fc2'] = x = Dense(4096, activation='relu', name='fc2')(x)
+        net['predictions'] = x = Dense(classes, activation='softmax', name='predictions')(x)
+    # else:
+    #     if pooling == 'avg':
+    #         x = GlobalAveragePooling2D()(x)
+    #     elif pooling == 'max':
+    #         x = GlobalMaxPooling2D()(x)
 
     # Ensure that the model takes into account
     # any potential predecessors of `input_tensor`.
