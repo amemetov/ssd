a1c1dee9f8e7cfe9164c56ad3a213984874fc70e
Arsen <arsen.memetov@aspose.com>
2018-01-31 23:13:08 +0200

    Implementing CreateMultiBoxHead.

--- keras_impl/ssd_pascal.py
+++ keras_impl/ssd_pascal.py
@@ -5,10 +5,13 @@ from keras.layers import Input, Cropping2D, Lambda
 from keras.layers import Conv2D, AtrousConv2D, MaxPooling2D
 from keras.layers import Dense, Activation, Flatten
 from keras.layers import Dropout, BatchNormalization, ELU
+from keras.layers import merge
 from keras.optimizers import Adam
 
 # from keras.applications import VGG16
 from .vgg import VGG16
+
+from .layers import L2Normalize
 
 
 def SSD300(base_net_name='vgg16', freeze_layers=None, use_bn=False):
@@ -25,18 +28,40 @@ def SSD300(base_net_name='vgg16', freeze_layers=None, use_bn=False):
     base_net_model = VGG16(ssd_net, ssd_net['image_input'], input_shape=input_dim,
                            include_top=False, weights='imagenet')
 
-    # predictions = addExtraLayers(base_net, use_bn=use_bn)
     addExtraLayers(ssd_net, base_net_model, use_bn=use_bn)
 
-    ssd_net['predictions'] = ssd_net['conv9_2']
-
     mbox_source_layers = ['conv4_3', 'fc7', 'conv6_2', 'conv7_2', 'conv8_2', 'conv9_2']
+    num_classes = 21
 
-    # mbox_layers = CreateMultiBoxHead(net, data_layer='data', from_layers=mbox_source_layers,
-    #                                  use_batchnorm=use_batchnorm, min_sizes=min_sizes, max_sizes=max_sizes,
-    #                                  aspect_ratios=aspect_ratios, steps=steps, normalizations=normalizations,
-    #                                  num_classes=num_classes, share_location=share_location, flip=flip, clip=clip,
-    #                                  prior_variance=prior_variance, kernel_size=3, pad=1, lr_mult=lr_mult)
+    layers_config = [
+    {'layer_width': 38, 'layer_height': 38, 'num_prior': 3,
+     'min_size':  30.0, 'max_size': None, 'aspect_ratios': [1.0, 2.0, 1/2.0]},
+
+    {'layer_width': 19, 'layer_height': 19, 'num_prior': 6,
+     'min_size':  60.0, 'max_size': 114.0, 'aspect_ratios': [1.0, 1.0, 2.0, 1/2.0, 3.0, 1/3.0]},
+
+    {'layer_width': 10, 'layer_height': 10, 'num_prior': 6,
+     'min_size': 114.0, 'max_size': 168.0, 'aspect_ratios': [1.0, 1.0, 2.0, 1/2.0, 3.0, 1/3.0]},
+
+    {'layer_width':  5, 'layer_height':  5, 'num_prior': 6,
+     'min_size': 168.0, 'max_size': 222.0, 'aspect_ratios': [1.0, 1.0, 2.0, 1/2.0, 3.0, 1/3.0]},
+
+    {'layer_width':  3, 'layer_height':  3, 'num_prior': 6,
+     'min_size': 222.0, 'max_size': 276.0, 'aspect_ratios': [1.0, 1.0, 2.0, 1/2.0, 3.0, 1/3.0]},
+
+    {'layer_width':  1, 'layer_height':  1, 'num_prior': 6,
+     'min_size': 276.0, 'max_size': 330.0, 'aspect_ratios': [1.0, 1.0, 2.0, 1/2.0, 3.0, 1/3.0]},
+]
+
+    mbox_layers = CreateMultiBoxHead(ssd_net, input_dim, layers_config,
+                                     flip=True, clip=False, prior_variance=[0.1, 0.1, 0.2, 0.2], offset=0.5,
+                                     num_classes=num_classes,
+                                     use_bn=use_bn,
+                                     kernel_size=3,
+                                     pad='same'#pad=1
+                                     )
+
+    ssd_net['predictions'] = merge(mbox_layers, mode='concat', concat_axis=2, name='predictions')
 
     model = Model(inputs=[ssd_net['image_input']], outputs=[ssd_net['predictions']])
 
@@ -45,10 +70,10 @@ def SSD300(base_net_name='vgg16', freeze_layers=None, use_bn=False):
     return model
 
 
-    # AddExtraLayers(net, use_batchnorm, lr_mult=lr_mult)
+    # AddExtraLayers(net, use_bn, lr_mult=lr_mult)
     #
     # mbox_layers = CreateMultiBoxHead(net, data_layer='data', from_layers=mbox_source_layers,
-    #                                  use_batchnorm=use_batchnorm, min_sizes=min_sizes, max_sizes=max_sizes,
+    #                                  use_bn=use_bn, min_sizes=min_sizes, max_sizes=max_sizes,
     #                                  aspect_ratios=aspect_ratios, steps=steps, normalizations=normalizations,
     #                                  num_classes=num_classes, share_location=share_location, flip=flip, clip=clip,
     #                                  prior_variance=prior_variance, kernel_size=3, pad=1, lr_mult=lr_mult)
@@ -110,149 +135,77 @@ def ConvBNLayer(net, tensor, layer_name, use_bn,
     return x
 
 
-def CreateMultiBoxHead(net, data_layer="data", num_classes=[], from_layers=[],
-                       use_objectness=False, normalizations=[], use_batchnorm=True, lr_mult=1,
-                       use_scale=True, min_sizes=[], max_sizes=[], prior_variance=[0.1],
-                       aspect_ratios=[], steps=[], img_height=0, img_width=0, share_location=True,
-                       flip=True, clip=True, offset=0.5, inter_layer_depth=[], kernel_size=1, pad=0,
-                       conf_postfix='', loc_postfix='', **bn_param):
+def CreateMultiBoxHead(net, img_size,
+                       layers_config,
+                       flip=True, clip=False, prior_variance=[0.1, 0.1, 0.2, 0.2], offset=0.5,
+                       num_classes=21,
+                       use_bn=True,
+                       kernel_size=1,
+                       pad='valid', #pad=0,
+                       **bn_param):
     assert num_classes, "must provide num_classes"
     assert num_classes > 0, "num_classes must be positive number"
-    if normalizations:
-        assert len(from_layers) == len(normalizations), "from_layers and normalizations should have same length"
-    assert len(from_layers) == len(min_sizes), "from_layers and min_sizes should have same length"
-    if max_sizes:
-        assert len(from_layers) == len(max_sizes), "from_layers and max_sizes should have same length"
-    if aspect_ratios:
-        assert len(from_layers) == len(aspect_ratios), "from_layers and aspect_ratios should have same length"
-    if steps:
-        assert len(from_layers) == len(steps), "from_layers and steps should have same length"
-    net_layers = net.keys()
-    assert data_layer in net_layers, "data_layer is not in net's layers"
-    if inter_layer_depth:
-        assert len(from_layers) == len(inter_layer_depth), "from_layers and inter_layer_depth should have same length"
 
-    num = len(from_layers)
+
+    num = len(layers_config)
     priorbox_layers = []
     loc_layers = []
     conf_layers = []
-    objectness_layers = []
-    for i in range(0, num):
-        from_layer = from_layers[i]
+    # for i in range(0, num):
+    for layer_config in layers_config:
+        layer = layer_config['layer']
+        normalization = layer_config['normalization']
+        num_priors_per_location = layer_config['num_prior']
+        min_size = layer_config['min_size']
+        max_size = layer_config['max_size']
+        aspect_ratios = layer_config['aspect_ratios']
 
         # Get the normalize value.
-        if normalizations:
-            if normalizations[i] != -1:
-                norm_name = "{}_norm".format(from_layer)
-                net[norm_name] = L.Normalize(net[from_layer],
-                                             scale_filler=dict(type="constant", value=normalizations[i]),
-                                             across_spatial=False, channel_shared=False)
-                from_layer = norm_name
+        if normalization != -1:
+            norm_name = "{}_norm".format(layer)
+            net[norm_name] = L2Normalize(scale=normalization, name=norm_name)(net[layer])
+            layer = norm_name
 
-        # Add intermediate layers.
-        if inter_layer_depth:
-            if inter_layer_depth[i] > 0:
-                inter_name = "{}_inter".format(from_layer)
-                ConvBNLayer(net, from_layer, inter_name, use_bn=use_batchnorm, use_relu=True, lr_mult=lr_mult,
-                            num_output=inter_layer_depth[i], kernel_size=3, pad=1, stride=1, **bn_param)
-                from_layer = inter_name
-
-        # Estimate number of priors per location given provided parameters.
-        min_size = min_sizes[i]
-        if type(min_size) is not list:
-            min_size = [min_size]
-        aspect_ratio = []
-        if len(aspect_ratios) > i:
-            aspect_ratio = aspect_ratios[i]
-            if type(aspect_ratio) is not list:
-                aspect_ratio = [aspect_ratio]
-        max_size = []
-        if len(max_sizes) > i:
-            max_size = max_sizes[i]
-            if type(max_size) is not list:
-                max_size = [max_size]
-            if max_size:
-                assert len(max_size) == len(min_size), "max_size and min_size should have same length."
-        if max_size:
-            num_priors_per_location = (2 + len(aspect_ratio)) * len(min_size)
-        else:
-            num_priors_per_location = (1 + len(aspect_ratio)) * len(min_size)
-        if flip:
-            num_priors_per_location += len(aspect_ratio) * len(min_size)
-        step = []
-        if len(steps) > i:
-            step = steps[i]
 
         # Create location prediction layer.
-        name = "{}_mbox_loc{}".format(from_layer, loc_postfix)
-        num_loc_output = num_priors_per_location * 4;
-        if not share_location:
-            num_loc_output *= num_classes
-        ConvBNLayer(net, from_layer, name, use_bn=use_batchnorm, use_relu=False, lr_mult=lr_mult,
-                    num_output=num_loc_output, kernel_size=kernel_size, pad=pad, stride=1, **bn_param)
-        permute_name = "{}_perm".format(name)
-        net[permute_name] = L.Permute(net[name], order=[0, 2, 3, 1])
-        flatten_name = "{}_flat".format(name)
-        net[flatten_name] = L.Flatten(net[permute_name], axis=1)
+        loc_name = "{}_mbox_loc".format(layer)
+        num_loc_output = num_priors_per_location * 4
+        x = ConvBNLayer(net, net[layer], loc_name, use_bn=use_bn,
+                    num_output=num_loc_output, kernel_size=kernel_size, pad=pad, stride=1)
+        #permute_name = "{}_perm".format(loc_name)
+        #net[permute_name] = L.Permute(net[loc_name], order=[0, 2, 3, 1])
+        flatten_name = "{}_flat".format(loc_name)
+        net[flatten_name] = Flatten(name=flatten_name)(x)
         loc_layers.append(net[flatten_name])
 
         # Create confidence prediction layer.
-        name = "{}_mbox_conf{}".format(from_layer, conf_postfix)
-        num_conf_output = num_priors_per_location * num_classes;
-        ConvBNLayer(net, from_layer, name, use_bn=use_batchnorm, use_relu=False, lr_mult=lr_mult,
-                    num_output=num_conf_output, kernel_size=kernel_size, pad=pad, stride=1, **bn_param)
-        permute_name = "{}_perm".format(name)
-        net[permute_name] = L.Permute(net[name], order=[0, 2, 3, 1])
-        flatten_name = "{}_flat".format(name)
-        net[flatten_name] = L.Flatten(net[permute_name], axis=1)
+        conf_name = "{}_mbox_conf".format(layer)
+        num_conf_output = num_priors_per_location * num_classes
+        x = ConvBNLayer(net, net[layer], conf_name, use_bn=use_bn,
+                    num_output=num_conf_output, kernel_size=kernel_size, pad=pad, stride=1)
+        #permute_name = "{}_perm".format(conf_name)
+        #net[permute_name] = L.Permute(net[conf_name], order=[0, 2, 3, 1])
+        flatten_name = "{}_flat".format(conf_name)
+        net[flatten_name] = Flatten(name=flatten_name)(x)
         conf_layers.append(net[flatten_name])
 
         # Create prior generation layer.
-        name = "{}_mbox_priorbox".format(from_layer)
-        net[name] = L.PriorBox(net[from_layer], net[data_layer], min_size=min_size,
-                               clip=clip, variance=prior_variance, offset=offset)
-        if max_size:
-            net.update(name, {'max_size': max_size})
-        if aspect_ratio:
-            net.update(name, {'aspect_ratio': aspect_ratio, 'flip': flip})
-        if step:
-            net.update(name, {'step': step})
-        if img_height != 0 and img_width != 0:
-            if img_height == img_width:
-                net.update(name, {'img_size': img_height})
-            else:
-                net.update(name, {'img_h': img_height, 'img_w': img_width})
-        priorbox_layers.append(net[name])
+        priorbox_name = "{}_mbox_priorbox".format(layer)
+        net[priorbox_name] = PriorBox(net, net[layer], priorbox_name,
+                                      img_size=img_size,
+                                      min_size=min_size, max_size=max_size, aspect_ratios=aspect_ratios,
+                                      flip=flip, clip=clip, variances=prior_variance, offset=offset)
+        # if step:
+        #     net.update(name, {'step': step})
+        priorbox_layers.append(net[priorbox_name])
 
-        # Create objectness prediction layer.
-        if use_objectness:
-            name = "{}_mbox_objectness".format(from_layer)
-            num_obj_output = num_priors_per_location * 2;
-            ConvBNLayer(net, from_layer, name, use_bn=use_batchnorm, use_relu=False, lr_mult=lr_mult,
-                        num_output=num_obj_output, kernel_size=kernel_size, pad=pad, stride=1, **bn_param)
-            permute_name = "{}_perm".format(name)
-            net[permute_name] = L.Permute(net[name], order=[0, 2, 3, 1])
-            flatten_name = "{}_flat".format(name)
-            net[flatten_name] = L.Flatten(net[permute_name], axis=1)
-            objectness_layers.append(net[flatten_name])
 
     # Concatenate priorbox, loc, and conf layers.
-    mbox_layers = []
-    name = "mbox_loc"
-    net[name] = L.Concat(*loc_layers, axis=1)
-    mbox_layers.append(net[name])
-    name = "mbox_conf"
-    net[name] = L.Concat(*conf_layers, axis=1)
-    mbox_layers.append(net[name])
-    name = "mbox_priorbox"
-    net[name] = L.Concat(*priorbox_layers, axis=2)
-    mbox_layers.append(net[name])
-    if use_objectness:
-        name = "mbox_objectness"
-        net[name] = L.Concat(*objectness_layers, axis=1)
-        mbox_layers.append(net[name])
+    net['mbox_loc'] = merge(loc_layers, mode='concat', concat_axis=1, name='mbox_loc')
+    net['mbox_conf'] = merge(conf_layers, mode='concat', concat_axis=1, name='mbox_conf')
+    net['mbox_priorbox'] = merge(priorbox_layers, mode='concat', concat_axis=1, name='mbox_priorbox')
 
-    return mbox_layers
+    return [net['mbox_loc'], net['mbox_conf'], net['mbox_priorbox']]
 
 
 def _freezeLayers(model, freeze_layers):
