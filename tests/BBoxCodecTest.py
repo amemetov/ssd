7289cb8f061319f1df4c808c2dc242dc3c55d15c
Arsen <arsen.memetov@aspose.com>
2018-03-26 19:15:50 +0300

    Adding tests for bbox_codec module.

---
+++ tests/BBoxCodecTest.py
@@ -0,0 +1,50 @@
+import unittest
+import numpy as np
+
+import prior_box as pb
+from bbox_codec import intersectionOverUnion, BBoxCodec
+
+class BBoxCodecTest(unittest.TestCase):
+    def test_iou(self):
+        self.assertEqual(0, intersectionOverUnion([0, 0, 10, 10], [10, 10, 20, 20]))
+
+        self.assertEqual(1, intersectionOverUnion([0, 0, 10, 10], [0, 0, 10, 10]))
+
+        self.assertEqual(1./7, intersectionOverUnion([0, 0, 10, 10], [5, 5, 15, 15]))
+        self.assertEqual(1./7, intersectionOverUnion([0, 0, 10, 10], [5, -5, 15, 5]))
+        self.assertEqual(1./7, intersectionOverUnion([0, 0, 10, 10], [-5, -5, 5, 5]))
+        self.assertEqual(1./7, intersectionOverUnion([0, 0, 10, 10], [-5, 5, 5, 15]))
+
+        self.assertEqual(9./25, intersectionOverUnion([0, 0, 10, 10], [2, 2, 8, 8]))
+        self.assertEqual(9./25, intersectionOverUnion([2, 2, 8, 8], [0, 0, 10, 10]))
+
+    def test_encode(self):
+        num_classes = 2
+        config_1x1 = [
+            {'layer_width': 1, 'layer_height': 1, 'num_prior': 6,
+             'min_size': 276.0, 'max_size': 330.0, 'aspect_ratios': [1.0, 1.0, 2.0, 1 / 2.0, 3.0, 1 / 3.0]},
+        ]
+        prior_boxes = pb.create_prior_boxes_vect(300, 300, config_1x1, pb.default_prior_variance)
+
+        self.assertEqual(6, len(prior_boxes), 'Expected 6 prior boxes')
+
+        bbox_codec = BBoxCodec(prior_boxes, num_classes)
+
+        # (num_gtb, 4 + num_classes)
+        y_orig = np.array([[0, 0.25, 1, 0.75, 1, 0], [0.25, 0, 0.75, 1, 0, 1]])
+
+        y_encoded_actual = bbox_codec.encode(y_orig)
+
+        # we expect 6 rows
+        # (num_priors, 4 + (1 + num_classes) + 4 + 4),
+        y_encoded_expected = np.array([
+            # (xmin, ymin, xmax, ymax, background_prob, class1, class2, gtb_mark, 7 zeros)
+            [0,    0,    0,    0,    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0,    0,    0,    0,    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0,    0,    0,    0,    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0,    0,    0,    0,    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0,    0.25, 1,    0.75, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+            [0.25, 0,    0.75, 1,    0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0]
+        ])
+
+        self.assertTrue(np.allclose(y_encoded_expected, y_encoded_actual), 'encoded Y is incorrect')
