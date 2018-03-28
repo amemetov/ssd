1acb434d275d33023046b81bab947deaf02c6eff
Arsen <arsen.memetov@aspose.com>
2018-03-28 14:47:22 +0300

    Replaced image loading call.

--- generator.py
+++ generator.py
@@ -2,10 +2,9 @@ import os
 import numpy as np
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
-from scipy.misc.pilutil import imread
-from PIL import Image as pil_image
 
 from bbox_codec import BBoxCodec
+import imaging
 
 
 class Generator(object):
@@ -35,11 +34,7 @@ class Generator(object):
 
                 for img_file_name in samples_batch:
                     img_full_path = os.path.join(self.img_dir, img_file_name)
-                    #img = load_img(img_full_path, target_size=self.image_size)
-                    #img = img_to_array(img)
-                    img = imread(img_full_path).astype('float32')
-                    #
-
+                    img = imaging.load_img(img_full_path).astype(np.float32)
 
                     # get the origin GTBs
                     y = self.gtb[img_file_name].copy()
@@ -54,70 +49,6 @@ class Generator(object):
                     y_batch.append(y_encoded)
 
                 yield x_batch, y_batch
-
-def load_img(path, grayscale=False, target_size=None):
-    """Loads an image into PIL format.
-
-    # Arguments
-        path: Path to image file
-        grayscale: Boolean, whether to load the image as grayscale.
-        target_size: Either `None` (default to original size)
-            or tuple of ints `(img_height, img_width)`.
-
-    # Returns
-        A PIL Image instance.
-
-    # Raises
-        ImportError: if PIL is not available.
-    """
-    if pil_image is None:
-        raise ImportError('Could not import PIL.Image. '
-                          'The use of `load_img` requires PIL.')
-    img = pil_image.open(path)
-    if grayscale:
-        if img.mode != 'L':
-            img = img.convert('L')
-    else:
-        if img.mode != 'RGB':
-            img = img.convert('RGB')
-    if target_size:
-        hw_tuple = (target_size[1], target_size[0])
-        if img.size != hw_tuple:
-            img = img.resize(hw_tuple)
-    return img
-
-def img_to_array(img, data_format='channels_last'):
-    """Converts a PIL Image instance to a Numpy array.
-
-    # Arguments
-        img: PIL Image instance.
-        data_format: Image data format.
-
-    # Returns
-        A 3D Numpy array.
-
-    # Raises
-        ValueError: if invalid `img` or `data_format` is passed.
-    """
-    # if data_format is None:
-    #     data_format = K.image_data_format()
-    if data_format not in {'channels_first', 'channels_last'}:
-        raise ValueError('Unknown data_format: ', data_format)
-    # Numpy array x has format (height, width, channel)
-    # or (channel, height, width)
-    # but original PIL image has format (width, height, channel)
-    x = np.asarray(img, dtype=np.float32)
-    if len(x.shape) == 3:
-        if data_format == 'channels_first':
-            x = x.transpose(2, 0, 1)
-    elif len(x.shape) == 2:
-        if data_format == 'channels_first':
-            x = x.reshape((1, x.shape[0], x.shape[1]))
-        else:
-            x = x.reshape((x.shape[0], x.shape[1], 1))
-    else:
-        raise ValueError('Unsupported image shape: ', x.shape)
-    return x
 
 
 if __name__ == '__main__':
