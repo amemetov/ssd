b2717b59c5525a2421ee8098f98771193dbcb3f9
Arsen <arsen.memetov@aspose.com>
2018-03-28 14:45:01 +0300

    Added imaging methods.

---
+++ imaging.py
@@ -0,0 +1,44 @@
+import numpy as np
+import cv2
+from scipy.misc.pilutil import imread
+
+def load_img(path):
+    # loads image as uint8
+    return imread(path)
+
+def randomize_brightness(img, brightness_delta=32):
+    # brightness = np.random.uniform(-brightness_delta, brightness_delta)
+    # img = np.clip(img + brightness, 0, 255)
+    # return img
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    brightness = -np.random.uniform(-brightness_delta, brightness_delta)
+    # V in range [0, 255]
+    hsv[:, :, 2] = np.clip(hsv[:, :, 2] + brightness, 0, 255)
+    return cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
+
+
+def randomize_contrast(img, contrast_lower=0.5, contrast_upper=1.5):
+    # contrast = np.random.uniform(contrast_lower, contrast_upper)
+    # img = np.clip(contrast * img, 0, 255)
+    # return img
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    contrast = np.random.uniform(-contrast_lower, contrast_upper)
+    # V in range [0, 255]
+    hsv[:, :, 2] = np.clip(hsv[:, :, 2] * contrast, 0, 255)
+    return cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
+
+def randomize_hue(img, hue_delta=18):
+    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    hue = np.random.uniform(-hue_delta, hue_delta)
+    # H in range [0, 360]
+    hsv[:, :, 0] = np.clip(hsv[:, :, 0] + hue, 0, 360)
+    return cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
+
+def randomize_saturation(img, saturation_lower=0.5, saturation_upper=1.5):
+    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    saturation = np.random.uniform(saturation_lower, saturation_upper)
+    # S in range [0, 1]
+    hsv[:, :, 1] = np.clip(hsv[:, :, 1] * saturation, 0, 1)
+    return cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
