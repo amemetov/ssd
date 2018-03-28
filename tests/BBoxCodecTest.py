e490b4128e8b17c9e296fea0e7a7a58c5551a145
Arsen <arsen.memetov@aspose.com>
2018-03-28 10:07:46 +0300

    Reworked BBoxCodec tests.

--- tests/BBoxCodecTest.py
+++ tests/BBoxCodecTest.py
@@ -20,18 +20,18 @@ class BBoxCodecTest(unittest.TestCase):
         self.assertEqual(9./25, intersectionOverUnion([2, 2, 8, 8], [0, 0, 10, 10]))
 
     def test_encode_iter(self):
-        start_time = time.time()
         self.__test_encode(False)
-        elapsed_time = time.time() - start_time
-        print("iter spent time: {}".format(elapsed_time))
-        # iter spent time: 0.07794070243835449
 
     def test_encode_vect(self):
-        start_time = time.time()
         self.__test_encode(True)
-        elapsed_time = time.time() - start_time
-        print("vect spent time: {}".format(elapsed_time))
-        # vect spent time: 0.00211334228515625
+
+    def test_perf_encode_iter(self):
+        # Iter Spent time: 0.07274351599335205
+        self.__test_perf_encode(False)
+
+    def test_perf_encode_vect(self):
+        # Vect Spent time: 0.0006487050122814253
+        self.__test_perf_encode(True)
 
     def __test_encode(self, use_vect):
         num_classes = 2
@@ -41,9 +41,6 @@ class BBoxCodecTest(unittest.TestCase):
         ]
         prior_boxes = pb.create_prior_boxes_vect(300, 300, config_1x1, pb.default_prior_variance)
         self.assertEqual(6, len(prior_boxes), 'Expected 6 prior boxes')
-
-        # for performance comparing can be used below prior_boxes
-        # prior_boxes = pb.create_prior_boxes_vect(300, 300, pb.default_config, pb.default_prior_variance)
 
         bbox_codec = BBoxCodec(prior_boxes, num_classes, use_vect=use_vect)
 
@@ -65,3 +62,18 @@ class BBoxCodecTest(unittest.TestCase):
         ])
 
         self.assertTrue(np.allclose(y_encoded_expected, y_encoded_actual), 'encoded Y is incorrect')
+
+
+    def __test_perf_encode(self, use_vect):
+        num_classes = 2
+
+        prior_boxes = pb.create_prior_boxes_vect(300, 300, pb.default_config, pb.default_prior_variance)
+        bbox_codec = BBoxCodec(prior_boxes, num_classes, use_vect=use_vect)
+
+        # (num_gtb, 4 + num_classes)
+        y_orig = np.array([[0, 0.25, 1, 0.75, 1, 0], [0.25, 0, 0.75, 1, 0, 1]])
+
+        start_time = time.perf_counter()
+        y_encoded_actual = bbox_codec.encode(y_orig)
+        elapsed_time = time.perf_counter() - start_time
+        print("{} Spent time: {}".format('Vect' if use_vect else 'Iter', elapsed_time))
