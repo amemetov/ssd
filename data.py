41be4318c5aaffdbb918415dca9fe713a31bc96b
Arsen <arsen.memetov@aspose.com>
2018-04-03 00:00:24 +0300

    Added background class into account

--- data.py
+++ data.py
@@ -52,13 +52,14 @@ class PascalVoc2012(object):
         return name in self.CLASSES
 
     def _to_one_hot(self, name):
-        nb_classes = len(self.CLASSES)
+        nb_classes = len(self.CLASSES) + 1 #for background
         one_hot_vector = [0] * nb_classes
         # method index throws error if there is no such item
-        ind = self.CLASSES.index(name)
+        ind = self.CLASSES.index(name) + 1 #for background
         one_hot_vector[ind] = 1
         return one_hot_vector
 
+# python data.py '../datasets/voc2012/VOCtrainval_11-May-2012/Annotations/' 'data/pascal_voc_2012-with-background.p'
 if __name__ == '__main__':
     import argparse
     import pickle
