83b7a9c53dfe976badda6d3f24b7b47ea8e4649e
Arsen <arsen.memetov@aspose.com>
2018-05-17 10:32:28 +0300

    Refactoring: removing unused code

--- ssd/bbox_codec.py
+++ ssd/bbox_codec.py
@@ -23,14 +23,16 @@ def intersectionOverUnion(gtb, pb):
     return iou
 
 class BBoxCodec(object):
-    def __init__(self, prior_boxes, num_classes, iou_threshold=0.5,
-                 encode_variances=True, use_vect=True):
+    def __init__(self, prior_boxes, num_classes, iou_threshold=0.5, encode_variances=True):
         self.prior_boxes = prior_boxes
         self.num_priors = len(prior_boxes)
         self.num_classes = num_classes
         self.iou_threshold = iou_threshold
         self.encode_variances = encode_variances
-        self.use_vect = use_vect
+
+        # add new axis for prior_boxes to manage broadcasting
+        self.pb_broadcasted = self.prior_boxes[:, np.newaxis]
+        self.pb_area = (self.pb_broadcasted[:, :, 2] - self.pb_broadcasted[:, :, 0]) * (self.pb_broadcasted[:, :, 3] - self.pb_broadcasted[:, :, 1])
 
     def encode(self, y_orig):
         """Convert Y from origin format to NN expected format
@@ -68,11 +70,7 @@ class BBoxCodec(object):
         if num_gtb == 0:
             return y_result
 
-        if self.use_vect:
-            self.__encode_vect(y_orig, y_result)
-            #self.__encode_vect_v1(y_orig, y_result)
-        else:
-            self.__encode_iter(y_orig, y_result)
+        self.__encode(y_orig, y_result)
 
         return y_result
 
@@ -118,7 +116,7 @@ class BBoxCodec(object):
 
         return bboxes
 
-    def __encode_vect(self, y_orig, y_result):
+    def __encode(self, y_orig, y_result):
         # 2D tensor, rows are  pbs(y_result)'s indices, columns gtb(y_orig)'s indices
         iou = self.__iou_full(y_orig)
 
@@ -179,135 +177,20 @@ class BBoxCodec(object):
         # set indicator to point that this PBs matched to GTBs - is used by SsdLoss
         y_result[pb_indices, -8] = 1
 
-    def __encode_vect_v1(self, y_orig, y_result):
-        # max_iou - 2D tensor (num_gtb, 2) where
-        # max_iou[:, 0] - idx of PB, max_iou[:, 1] - iou value itself
-        max_iou = np.apply_along_axis(self.__find_most_overlapped_pb_vect, 1, y_orig)
-        threshold_mask = max_iou[:, 1] > self.iou_threshold
-        pb_indices = max_iou[threshold_mask, 0].astype(np.int)
-
-        gt_boxes = y_orig[threshold_mask]
-        box_center = 0.5 * (gt_boxes[:, :2] + gt_boxes[:, 2:4])
-        box_wh = gt_boxes[:, 2:4] - gt_boxes[:, :2]
-
-        prior_boxes = self.prior_boxes[pb_indices]
-        pb_center = 0.5 * (prior_boxes[:, :2] + prior_boxes[:, 2:4])
-        pb_wh = prior_boxes[:, 2:4] - prior_boxes[:, :2]
-
-        # see loss computing in the origin SSD paper
-        y_result[pb_indices, :2] = (box_center - pb_center) / pb_wh
-        y_result[pb_indices, 2:4] = np.log(box_wh / pb_wh)
-
-        if self.encode_variances:
-            # encode variance of cx, cy
-            y_result[pb_indices, :2] /= prior_boxes[:, -4:-2]
-            # encode variance of w, h
-            y_result[pb_indices, 2:4] /= prior_boxes[:, -2:]
-
-        # probability of the background_class is 0
-        y_result[pb_indices, 4] = 0.0
-
-        # copy probabilities of categories
-        y_result[pb_indices, 5:-8] = gt_boxes[:, 4:]
-
-        # set indicator to point that this PB matched to GTB - is used by SsdLoss
-        y_result[pb_indices, -8] = 1
-
-    def __encode_iter(self, y_orig, y_result):
-        for gtb in y_orig:
-            pb_idx = self.__find_most_overlapped_pb_iter(gtb)
-            if pb_idx is not None:
-                box = gtb[:4]
-                box_center = 0.5 * (box[:2] + box[2:4])
-                box_wh = box[2:4] - box[:2]
-
-                pb = self.prior_boxes[pb_idx]
-                pb_center = 0.5 * (pb[:2] + pb[2:4])
-                pb_wh = pb[2:4] - pb[:2]
-
-                # see loss computing in the origin SSD paper
-                y_result[pb_idx][:2] = (box_center - pb_center) / pb_wh
-                y_result[pb_idx][2:4] = np.log(box_wh / pb_wh)
-
-                if self.encode_variances:
-                    # encode variance of cx, cy
-                    y_result[pb_idx][:2] /= pb[-4:-2]
-                    # encode variance of w, h
-                    y_result[pb_idx][2:4] /= pb[-2:]
-
-                # probability of the background_class is 0
-                y_result[pb_idx][4] = 0.0
-
-                # copy probabilities of categories
-                y_result[pb_idx][5:-8] = gtb[4:]
-
-                # set indicator to point that this PB matched to GTB - is used by SsdLoss
-                y_result[pb_idx][-8] = 1
-
-    def __find_most_overlapped_pb_vect(self, gtb):
-        # calc iou with each prior_boxes in one step
-        iou = self.__iou_vect(gtb)
-        max_idx = np.argmax(iou)
-        return max_idx, iou[max_idx]
-
     def __iou_full(self, gtbs):
-        # add new axis for prior_boxes to manage broadcasting
-        pbs = self.prior_boxes[:, np.newaxis]
-
         # find left-top and right-bottom of intersection
-        left = np.maximum(gtbs[:, 0], pbs[:, :, 0])
-        top = np.maximum(gtbs[:, 1], pbs[:, :, 1])
-        right = np.minimum(gtbs[:, 2], pbs[:, :, 2])
-        bottom = np.minimum(gtbs[:, 3], pbs[:, :, 3])
+        left = np.maximum(gtbs[:, 0], self.pb_broadcasted[:, :, 0])
+        top = np.maximum(gtbs[:, 1], self.pb_broadcasted[:, :, 1])
+        right = np.minimum(gtbs[:, 2], self.pb_broadcasted[:, :, 2])
+        bottom = np.minimum(gtbs[:, 3], self.pb_broadcasted[:, :, 3])
 
         # area of intersection
         inter_area = (right - left) * (bottom - top)
 
         gtb_area = (gtbs[:, 2] - gtbs[:, 0]) * (gtbs[:, 3] - gtbs[:, 1])
-        # TODO: move calculating pb_area to __init and reuse it
-        pb_area = (pbs[:, :, 2] - pbs[:, :, 0]) * (pbs[:, :, 3] - pbs[:, :, 1])
 
-        union_area = gtb_area + pb_area - inter_area
+        union_area = gtb_area + self.pb_area - inter_area
 
         # 2D tensor, rows are  pbs(y_result)'s indices, columns gtb(y_orig)'s indices
         iou = np.divide(inter_area, union_area, out=np.zeros_like(union_area), where=union_area != 0)
         return iou
-
-    def __iou_vect(self, gtb):
-        # find left-top and right-bottom of intersection
-        left = np.maximum(gtb[0], self.prior_boxes[:, 0])
-        top = np.maximum(gtb[1], self.prior_boxes[:, 1])
-        right = np.minimum(gtb[2], self.prior_boxes[:, 2])
-        bottom = np.minimum(gtb[3], self.prior_boxes[:, 3])
-
-        # area of intersection
-        inter_area = (right - left) * (bottom - top)
-
-        gtb_area = (gtb[2] - gtb[0]) * (gtb[3] - gtb[1])
-        # TODO: move calculating pb_area to __init and reuse it
-        pb_area = (self.prior_boxes[:, 2] - self.prior_boxes[:, 0]) * (self.prior_boxes[:, 3] - self.prior_boxes[:, 1])
-
-        union_area = gtb_area + pb_area - inter_area
-
-        iou = np.divide(inter_area, union_area, out=np.zeros_like(union_area), where=union_area != 0)
-        return iou
-
-    def __find_most_overlapped_pb_iter(self, gtb):
-        """
-
-        # Arguments
-            gtb: tensor of shape (4,) containing (xmin, ymin, xmax, ymax)
-        """
-
-        # find the most overlapped PB with iou(gtb, pb) > iou_threshold
-        max_iou = 0
-        most_overlapped_pb_idx = None
-        for pb_idx in range(self.num_priors):
-            pb =self.prior_boxes[pb_idx]
-            iou = intersectionOverUnion(gtb, pb)
-            if iou > self.iou_threshold and iou > max_iou:
-                max_iou = iou
-                most_overlapped_pb_idx = pb_idx
-
-        return most_overlapped_pb_idx
-
--- tests/BBoxCodecTest.py
+++ tests/BBoxCodecTest.py
@@ -19,21 +19,7 @@ class BBoxCodecTest(unittest.TestCase):
         self.assertEqual(9./25, intersectionOverUnion([0, 0, 10, 10], [2, 2, 8, 8]))
         self.assertEqual(9./25, intersectionOverUnion([2, 2, 8, 8], [0, 0, 10, 10]))
 
-    def test_encode_iter(self):
-        self.__test_encode(False)
-
-    def test_encode_vect(self):
-        self.__test_encode(True)
-
-    def test_perf_encode_iter(self):
-        # Iter Spent time: 0.07274351599335205
-        self.__test_perf_encode(False)
-
-    def test_perf_encode_vect(self):
-        # Vect Spent time: 0.0006487050122814253
-        self.__test_perf_encode(True)
-
-    def __test_encode(self, use_vect):
+    def test_encode(self):
         num_classes = 2
         config_1x1 = [
             {'layer_width': 1, 'layer_height': 1, 'num_prior': 6,
@@ -42,43 +28,40 @@ class BBoxCodecTest(unittest.TestCase):
         prior_boxes = pb.create_prior_boxes_vect(300, 300, config_1x1, pb.default_prior_variance)
         self.assertEqual(6, len(prior_boxes), 'Expected 6 prior boxes')
 
-        bbox_codec = BBoxCodec(prior_boxes, num_classes+1, use_vect=use_vect)
+        # expected_pbs = np.array([
+        #     [0.04,  0.04,  0.96,  0.96,  0.1, 0.1, 0.2, 0.2],
+        #     [0,     0,     1,     1,     0.1, 0.1, 0.2, 0.2],
+        #     [0,     0.175, 1,     0.825, 0.1, 0.1, 0.2, 0.2],
+        #     [0.175, 0,     0.825, 1,     0.1, 0.1, 0.2, 0.2],
+        #     [0,     0.234, 1,     0.766, 0.1, 0.1, 0.2, 0.2],
+        #     [0.234, 0,     0.766, 1,     0.1, 0.1, 0.2, 0.2]
+        # ])
+
+        bbox_codec = BBoxCodec(prior_boxes, num_classes+1)
 
         # (num_gtb, 4 + num_classes)
         y_orig = np.array([[0, 0.25, 1, 0.75, 1, 0], [0.25, 0, 0.75, 1, 0, 1]])
 
+        # (num_priors, 4 + (1 + num_classes) + 4 + 4),
         y_encoded_actual = bbox_codec.encode(y_orig)
 
-        # we expect 6 rows
-        # (num_priors, 4 + (1 + num_classes) + 4 + 4),
-        y_encoded_expected = np.array([
-            # (xmin, ymin, xmax, ymax, background_prob, class1, class2, gtb_mark, 7 zeros)
-            [0,    0,    0,    0,    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0,    0,    0,    0,    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0,    0,    0,    0,    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0,    0,    0,    0,    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-
-            #[0,    0.25, 1,    0.75,    0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            #[0.25, 0,    0.75, 1,       0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0]
-
-            #
-            [0, 0, 0, -0.0604594272868,  0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0,  -0.0604594272868, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0]
-
-            # when encoded variances
-            # [0, 0, 0, -0.30229714,  0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            # [0, 0, -0.30229714, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0]
-
+        # (num_priors, [bg_prob, class1_prob, class2_prob])
+        y_expected_assigned_classes = np.array([
+            [0, 1, 0],
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1],
+            [0, 1, 0],
+            [0, 0, 1]
         ])
 
-        self.assertTrue(np.allclose(y_encoded_expected, y_encoded_actual), 'encoded Y is incorrect')
+        self.assertTrue(np.allclose(y_expected_assigned_classes, y_encoded_actual[:, 4:4+3]), 'encoded Y is incorrect')
 
-
-    def __test_perf_encode(self, use_vect):
+    def test_perf_encode(self):
         num_classes = 2
 
         prior_boxes = pb.create_prior_boxes_vect(300, 300, pb.default_config, pb.default_prior_variance)
-        bbox_codec = BBoxCodec(prior_boxes, num_classes+1, use_vect=use_vect)
+        bbox_codec = BBoxCodec(prior_boxes, num_classes+1)
 
         # (num_gtb, 4 + num_classes)
         y_orig = np.array([[0, 0.25, 1, 0.75, 1, 0], [0.25, 0, 0.75, 1, 0, 1]])
@@ -86,4 +69,4 @@ class BBoxCodecTest(unittest.TestCase):
         start_time = time.perf_counter()
         y_encoded_actual = bbox_codec.encode(y_orig)
         elapsed_time = time.perf_counter() - start_time
-        print("{} Spent time: {}".format('Vect' if use_vect else 'Iter', elapsed_time))
+        print("Spent time: {}".format(elapsed_time))
