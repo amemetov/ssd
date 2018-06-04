099b62235ff8d627bb67107f06a8a43662ece613
Arsen <arsen.memetov@aspose.com>
2018-06-04 11:05:45 +0300

    Implemented method show_predictions

--- ssd/tools.py
+++ ssd/tools.py
@@ -45,14 +45,13 @@ def show_categories(images, predictions, num_classes, cols, conf_threshold=None,
     fig, axes = plt.subplots(rows, cols, figsize=figsize)
 
     for img, confs, (i, ax) in zip(images, predictions, enumerate(axes.flat)):
+        ax.imshow(img)
+
         if conf_threshold is None:
             cat_id = np.argmax(confs)
             cat_conf = confs[cat_id]
 
-            __show_categories(ax, img, [cat_id], [cat_conf], colors, font_size)
-
-            # ax.imshow(img)
-            # text = ax.text(0, 0, label, verticalalignment='top', fontsize=font_size, bbox={'facecolor': color, 'alpha': 0.5})
+            __show_categories(ax, [cat_id], [cat_conf], colors, font_size)
         else:
             mask = confs > conf_threshold
             thresholded_categories = np.nonzero(mask)[0]
@@ -63,13 +62,11 @@ def show_categories(images, predictions, num_classes, cols, conf_threshold=None,
             thresholded_categories = thresholded_categories[sort_indices]
             thresholded_confs = thresholded_confs[sort_indices]
 
-            __show_categories(ax, img, thresholded_categories, thresholded_confs, colors, font_size)
+            __show_categories(ax, thresholded_categories, thresholded_confs, colors, font_size)
 
     plt.tight_layout()
 
-def __show_categories(ax, img, categories, confs, colors, font_size):
-    ax.imshow(img)
-
+def __show_categories(ax, categories, confs, colors, font_size, x=0, y=0):
     text = ''
     for cat, conf in zip(categories, confs):
         label_idx = int(cat - 1)# take background into account
@@ -77,7 +74,7 @@ def __show_categories(ax, img, categories, confs, colors, font_size):
         color = colors[label_idx]
         text = text + label + '\n'
 
-    ax.text(0, 0, text, verticalalignment='top', fontsize=font_size, bbox={'facecolor': color, 'alpha': 0.5})
+    ax.text(x, y, text, verticalalignment='top', fontsize=font_size, bbox={'facecolor': color, 'alpha': 0.5})
 
 def show_bboxes(images, bboxes, num_classes, cols, figsize=(12, 8)):
     if len(images) != len(bboxes):
@@ -90,7 +87,7 @@ def show_bboxes(images, bboxes, num_classes, cols, figsize=(12, 8)):
     for img, boxes, (i, ax) in zip(images, bboxes, enumerate(axes.flat)):
         # when just one box is presented - convert it to array
         if boxes.ndim == 1:
-            boxes = np.array([boxes])
+            boxes = np.expand_dims(boxes, 0)#np.array([boxes])
 
         img_w, img_h = img.shape[1], img.shape[0]
         colors = plt.cm.hsv(np.linspace(0, 1, len(boxes))).tolist()
@@ -106,7 +103,58 @@ def show_bboxes(images, bboxes, num_classes, cols, figsize=(12, 8)):
             coords = (xmin, ymin), xmax - xmin + 1, ymax - ymin + 1
             patch = ax.add_patch(plt.Rectangle(*coords, fill=False, edgecolor=color, linewidth=2))
 
-def show_predictions(img, predictions, num_classes, figsize=(12, 8), ax=None, font_size=14):
+def show_predictions(images, predictions, num_classes, cols, conf_threshold=None, figsize=(12, 8), ax=None, font_size=14):
+    if len(images) != len(predictions):
+        raise ValueError("The size of 'images' list should be equal to the size of 'predictions' list")
+
+    if conf_threshold is not None and not 0 <= conf_threshold <= 1:
+        raise ValueError("'conf_threshold' should be either None or in range [0, 1]")
+
+    nb_images = len(images)
+    rows = nb_images // cols
+
+    category_colors = plt.cm.hsv(np.linspace(0, 1, num_classes)).tolist()
+
+    fig, axes = plt.subplots(rows, cols, figsize=figsize)
+
+    for img, img_preds, (i, ax) in zip(images, predictions, enumerate(axes.flat)):
+        if img_preds.ndim == 1:
+            img_preds = np.expand_dims(img_preds, 0)#np.array([img_preds])
+
+        img_w, img_h = img.shape[1], img.shape[0]
+        bbox_colors = plt.cm.hsv(np.linspace(0, 1, len(img_preds))).tolist()
+
+        ax.imshow(img)
+
+        for pred, bbox_color in zip(img_preds, bbox_colors):
+            bbox = np.clip(pred[:4], 0, 1)
+            confs = pred[4:]
+
+            xmin, ymin, xmax, ymax = int(bbox[0] * img_w), int(bbox[1] * img_h), int(bbox[2] * img_w), int(bbox[3] * img_h)
+            coords = (xmin, ymin), xmax - xmin + 1, ymax - ymin + 1
+            ax.add_patch(plt.Rectangle(*coords, fill=False, edgecolor=bbox_color, linewidth=2))
+
+            if conf_threshold is None:
+                cat_id = np.argmax(confs)
+                cat_conf = confs[cat_id]
+
+                __show_categories(ax, [cat_id], [cat_conf], category_colors, font_size, xmin, ymin)
+            else:
+                mask = confs > conf_threshold
+                thresholded_categories = np.nonzero(mask)[0]
+                thresholded_confs = confs[mask]
+
+                # sort in desc order
+                sort_indices = np.argsort(thresholded_confs)[::-1]
+                thresholded_categories = thresholded_categories[sort_indices]
+                thresholded_confs = thresholded_confs[sort_indices]
+
+                __show_categories(ax, thresholded_categories, thresholded_confs, category_colors, font_size, xmin, ymin)
+
+    plt.tight_layout()
+
+
+def show_detections(img, predictions, num_classes, figsize=(12, 8), ax=None, font_size=14):
     if not ax:
         fig, ax = plt.subplots(figsize=figsize)
 
@@ -188,3 +236,11 @@ def make_prediction(model, target_img_size, base_dir, test_files):
     preprocessed_images = np.array(preprocessed_images)
     y_pred = model.predict(preprocessed_images)
     return orig_images, preprocessed_images, y_pred
+
+def freeze_model(model, freeze_start_layer_name):
+    #model.trainable = True
+    trainable = False
+    for layer in model.layers:
+        if layer.name == freeze_start_layer_name:
+            trainable = True
+        layer.trainable = trainable
